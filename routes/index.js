9c6a11b2d7b2eba0265a56dccf5299538e5e950b
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2020-12-17 22:46:44 +0530

    Added some things
    Added the ability to view encoder settings and view/reset stream key

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const User = require('../models/User');
+const cryptoRandomString = require('crypto-random-string');
 
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
@@ -11,23 +12,52 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   User.findOne({ username: req.user.username }).then(useraccount => {
     res.render('dashboard', {
       user: req.user,
-      streamtitle: useraccount.stream_title
+      streamtitle: useraccount.stream_title,
+      streamkey: useraccount.stream_key
     })
   })
 );
-router.post('/dashboard', (req, res) => {
-  const streamtitle = req.body.streamtitle
-
-  // Check if stream has been updated
+router.post('/dashboard/streamkey', (req, res) => {
   User.findOne({ username: req.user.username }, (err, user) => {
-    user.stream_title = streamtitle;
+    user.stream_key = cryptoRandomString({ length: 20, type: 'alphanumeric' });
     user.save(function(err, user) {
       req.flash(
         'success_msg',
-        'Changes succesfully updated.'
+        'Stream Key succesfully reset.'
       );
       res.redirect('/dashboard');
     })
-  })
+  });
+})
+router.post('/dashboard', (req, res) => {
+  const streamtitle = req.body.streamtitle
+
+  if (streamtitle.length > 3){
+    if (streamtitle.length <= 61){
+      // Check if stream has been updated
+      User.findOne({ username: req.user.username }, (err, user) => {
+        user.stream_title = streamtitle;
+        user.save(function(err, user) {
+          req.flash(
+            'success_msg',
+            'Changes succesfully updated.'
+          );
+          res.redirect('/dashboard');
+        })
+      });
+    } else {
+      req.flash(
+        'error_msg',
+        'Stream title must not be longer than 61 Characters.'
+      );
+      res.redirect('/dashboard');
+    }
+  } else {
+    req.flash(
+      'error_msg',
+      'Stream title must be longer than 3 Characters.'
+    );
+    res.redirect('/dashboard');
+  }
 })
 module.exports = router;
