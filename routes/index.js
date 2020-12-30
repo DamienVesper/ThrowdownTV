c0da62af186cd626aeed8d256d4b60af6e7d0642
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2020-12-31 00:03:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -231,9 +231,7 @@ router.get('/:username', (req, res) => {
       let vipstatus = `[VIP]`
       let staffstatus = `[STAFF]`
       if (user.banned) return res.render('banned');
-      if (!user.isStaff) {
-        staffstatus = " "
-      }
+      
       if (!user.isVip) {
         vipstatus = " "
       }
