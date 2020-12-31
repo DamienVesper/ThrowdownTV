e091f77b4da5021ff9876ba9c98202f03c9abd42
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-01-01 05:19:36 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb://throwdown:adc71a12432bf13f51cf010707e80b15@144.91.76.32/throwdown?authMechanism=DEFAULT';
+dbPassword = 'mongodb://throwdown:adc71a12432bf13f51cf010707e80b15@144.91.76.32/admin';
 
 module.exports = {
     mongoURI: dbPassword
