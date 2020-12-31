cae866057bc0eb4890ab93e5322bcc701b0aee45
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-01-01 05:12:48 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://throwdown:'+ encodeURIComponent('adc71a12432bf13f51cf010707e80b15') + '@144.91.76.32/throwdown?retryWrites=true&w=majority';
+dbPassword = 'mongodb://throwdown:'+ encodeURIComponent('adc71a12432bf13f51cf010707e80b15') + '@144.91.76.32/throwdown?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
