4ebc4a569acbd47689cc9552a44bceeb5e157198
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-01-01 05:37:47 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -23,7 +23,7 @@ router.get('/streamkey/:streamkey', (req, res) =>
 // Email verification check
 router.get('/email_verify/:emailverificationkey', async (req, res) =>
     await User.findOne({ email_verification_key: req.params.emailverificationkey }).then(useraccount => {
-        if (useraccount.verification_status === true) {
+        if (useraccount.verification_status) {
             req.flash(
                 'error_msg',
                 'Email Already Verified.'
