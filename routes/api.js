e0fe8ed147262c9995b2a0372070f3a763080760
sev-ofc <47048628+SeverePain@users.noreply.github.com>
2020-12-31 16:32:43 -0800

    Turned this into an Async function

--- routes/api.js
+++ routes/api.js
@@ -21,9 +21,10 @@ router.get('/streamkey/:streamkey', (req, res) =>
     })
 );
 // Email verification check
-router.get('/email_verify/:emailverificationkey', (req, res) =>
-    User.findOne({ email_verification_key: req.params.emailverificationkey }).then(useraccount => {
-        if (useraccount.verification_status === true) {
+router.get('/email_verify/:emailverificationkey', async (req, res) => {
+
+    let useraccount = await User.findOne({email_verification_key: req.params.emailverificationkey});
+        if(useraccount.verification_status == true) {
             req.flash(
                 'error_msg',
                 'Email Already Verified.'
@@ -38,13 +39,10 @@ router.get('/email_verify/:emailverificationkey', (req, res) =>
                 );
                 res.redirect('/users/login');
             })
-        }
-    }).catch(err => {
-        req.flash(
-            'error_msg',
-            'UNKNOWN ERROR.'
-        );
+
+
         res.redirect('/users/login');
-    })
-);
+    }
+    
+});
 module.exports = router;
