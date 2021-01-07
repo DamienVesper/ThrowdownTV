f75544dfa95b84ecfd71574978113155e025792f
sev-ofc <47048628+SeverePain@users.noreply.github.com>
2021-01-06 21:25:00 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -230,11 +230,11 @@ router.get('/:username', (req, res) => {
       let badge = ``
       if (user.banned) return res.render('banned', {banreason: "Ya done goofed buddy..."});
       if (user.isStaff && user.isVip) {
-        badge = `<img src="https://chat.throwdown.tv:8443/img/staff" alt="[THROWDOWN STAFF]" title="Throwdown Verified Staff" width="20" height="20">`
+        badge = `<img src="https://chat.throwdown.tv:8443/img/staff" alt="[THROWDOWN STAFF]" title="Throwdown Verified Staff" width="20" height="20">    `
       } else if (user.isVip && !user.isStaff) {
-        badge = `<img src="https://chat.throwdown.tv:8443/img/vip" alt="[VIP]" title="VIP" width="20" height="20">`
+        badge = `<img src="https://chat.throwdown.tv:8443/img/vip" alt="[VIP]" title="VIP" width="20" height="20">    `
       } else if (user.isStaff && !user.isVip) {
-        badge = `<img src="https://chat.throwdown.tv:8443/img/staff" alt="[THROWDOWN STAFF]" title="Throwdown Verified Staff" width="20" height="20">`
+        badge = `<img src="https://chat.throwdown.tv:8443/img/staff" alt="[THROWDOWN STAFF]" title="Throwdown Verified Staff" width="20" height="20">    `
       }
       
       if (req.isAuthenticated()) {
