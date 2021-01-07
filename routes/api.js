9ce973c52951bf76db6ead773ad313a17f757dee
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-01-07 12:52:11 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -24,7 +24,7 @@ router.get('/streamkey/:streamkey', (req, res) =>
 // Email verification check
 router.get('/email_verify/:emailverificationkey', async (req, res) => {
     let useraccount = await User.findOne({email_verification_key: req.params.emailverificationkey});
-    axios.get('http://cdn.throwdown.tv/api/streams/' + user.username)
+    axios.get('http://cdn.throwdown.tv/api/streams/' + req.params.emailverificationkey)
         .then(function (response) {
             if (response.data.verification_status) {
                 req.flash(
