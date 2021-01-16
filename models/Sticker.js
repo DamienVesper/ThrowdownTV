fbd9dd271360ccc2b68a52c4df6e5e51e6b8cd9c
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-01-16 14:24:55 +0530

    Added Schema for stickers

---
+++ models/Sticker.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const StickerSchema = new mongoose.Schema({
+    stickername: {
+        type: String,
+        required: true
+    },
+    stickerurl: {
+        type: String,
+        required: true
+    },
+    stickerowner: {
+        type: String,
+        required: true
+    },
+});
+
+const User = mongoose.model('Sticker', StickerSchema);
+
+module.exports = User;
