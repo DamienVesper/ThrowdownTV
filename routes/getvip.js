5c5118af8d7d98aae913ceccfeb7b2ced1ccecfd
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-01-17 12:24:14 +0530

    Update getvip.js

--- routes/getvip.js
+++ routes/getvip.js
@@ -4,15 +4,6 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const User = require('../models/User');
 const config = require('../config.json')
 
-paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': 'AThsb8-yGjoibhatKQhxfhNmmTvr1YhVkBI7KOBEq2lRQ0Srt8R4vQDb1fA4NztTLvCWs0idierlr_nZ',
-    'client_secret': 'EJlbgcjSf62KiQbUzMLjTVcLicBaJb46KSUc0a78kugUR1czqEu69Mlp-m9gwZlCc8mTaDi6SrVBDtTV',
-    'headers' : {
-		'custom': 'header'
-    }
-});
-
 //get vip page
 router.get('/info', ensureAuthenticated, (req, res) => {
     res.render('getvip', {
