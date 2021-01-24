3c0a90fde92c36ef6b8a58ad188a8a2551eaab65
Damien Vesper <ldamienvesper@gmail.com>
2021-01-24 08:52:46 -0600

    add some express ejs templates

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,30 @@
 const express = require(`express`);
 const router = express.Router();
 
-router.get(`/`, (req, res) => res.render(`index.ejs`));
+const User = require(`../models/user.model.js`);
+
+// Landing page.
+router.get(`/`, (req, res) => {
+    req.isAuthenticated()
+        ? res.redirect(`/browse`)
+        : res.render(`welcome.ejs`);
+});
+
+// Terms of Service.
+router.get(`/tos`, (req, res) => res.render(`tos.ejs`));
+
+// Browsing.
+router.get(`/browse`, async(req, res) => {
+    const streamers = [];
+    const streamerData = await User.find({ live: true });
+
+    for (const streamer of streamerData) streamers.push(streamer.username);
+    res.render(`browse.ejs`, { streamers });
+});
+
+// Following.
+router.get(`/following`, async (req, res) => {
+    if (!req.isAuthenticated()) res.redirect(`/login`);
+});
 
 module.exports = router;
