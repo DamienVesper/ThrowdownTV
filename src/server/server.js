0b4f19fd81d8e880753021ad1530dce7ef691dc0
Damien Vesper <ldamienvesper@gmail.com>
2021-01-24 10:18:31 -0600

    fix comment

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const log = require(`./utils/log.js`);
 const fs = require(`fs`);
 const path = require(`path`);
 
-// Socket.io
+// Socket.IO server.
 const io = require(`./socket.js`);
 
 // HTTP / HTTPS transport protocols.
