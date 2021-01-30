479c1333f8934da42eeade0622e67e4c6f7adabc
Damien Vesper <ldamienvesper@gmail.com>
2021-01-29 20:08:50 -0600

    add serverside verification of data

--- src/server/socket.js
+++ src/server/socket.js
@@ -6,6 +6,9 @@ const fs = require(`fs`);
 const http = require(`http`);
 const https = require(`https`);
 
+const User = require(`./models/user.model.js`);
+const Ban = require(`./models/ban.model.js`);
+
 // Configure socket.
 const server = config.mode === `prod`
     ? https.createServer({
@@ -26,11 +29,25 @@ const io = require(`socket.io`)(server, {
 server.listen(config.socketPort, () => log(`green`, `Socket.IO bound to port ${config.socketPort}.`));
 
 // Handle new connections.
-io.on(`connection`, socket => {
+io.on(`connection`, async socket => {
     log(`magenta`, `A new user has connected to the chat! | IP: ${socket.handshake.address} | Origin: ${socket.request.headers.origin}.`);
 
-    // Send a handshake back to the client to let them know that we have connected.
-    socket.emit(`handshake`);
+    socket.on(`connectToChat`, async (username, token, streamerUsername) => {
+        const user = await User.findOne({ username: username });
+        const streamer = await User.findOne({ username: streamerUsername });
+
+        // If the user or the token is incorrect, disconnect the socket.
+        if (!user || user.token !== token || !streamer) {
+            log(`cyan`, `Fraudential credentials provided. Disconnecting IP: ${socket.handshake.address}.`);
+            return socket.disconnect();
+        }
+
+        const chatter = socket;
+        chatter.channel = streamerUsername;
+
+        // Send a handshake back to the client to let them know that we have connected.
+        socket.emit(`handshake`);
+    });
 });
 
 module.exports = io;
