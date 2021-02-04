4001be6a37e2e42e1333cfd6edf7d62a5ec0fdfb
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-02-04 18:05:48 +0530

    DISPLAY NAME HAS BEEN WORKED ON

--- src/server/routes/post.js
+++ src/server/routes/post.js
@@ -26,18 +26,17 @@ router.post(`/dashboard`, async (req, res) => {
 router.post(`/accountoptions`, async (req, res) => {
     if (!req.isAuthenticated()) return res.redirect(`/login`);
 
-    if (!req.body[`stream-title`] || !req.body[`stream-description`] || !req.body[`donation-link`] ||
-    typeof req.body[`stream-title`] !== `string` || typeof req.body[`stream-description`] !== `string` || typeof req.body[`donation-link`] !== `string`) return res.json({ errors: `Please fill out all fields` });
+    if (!req.body[`display-name`] || typeof req.body[`display-name`] !== `string`) return res.json({ errors: `Please fill out all fields` });
+
+    if (req.body[`display-name`].toLowerCase() !== req.user.username) return res.json({ errors: `Display Name must match the Username` });
 
     const user = await User.findOne({ username: req.user.username });
 
-    user.settings.title = req.body[`stream-title`].substr(0, 80);
-    user.settings.description = req.body[`stream-description`].substr(0, 1000);
-    user.settings.donationLink = req.body[`donation-link`].substr(0, 128);
+    user.displayName = req.body[`display-name`];
 
     user.save(err => {
-        if (err) return res.json({ errors: `Invalid user data` });
-        return res.json({ success: `Succesfully updated stream data.` });
+        if (err) return res.json({ errors: `Invalid account data` });
+        return res.json({ success: `Succesfully updated account data.` });
     });
 });
 
