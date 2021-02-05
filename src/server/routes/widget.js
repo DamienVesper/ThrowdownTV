892d5c2f25435a631d1bb8361eec376d0f1efbb2
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-02-05 13:07:43 +0530

    This is broken

--- src/server/routes/widget.js
+++ src/server/routes/widget.js
@@ -7,7 +7,8 @@ router.get(`/chat/:streamer`, async (req, res) => {
     const streamerData = await User.findOne({ username: req.params.streamer.toLowerCase() });
 
     if (!streamerData) return res.render(`404.ejs`);
-    res.render(`chatWidget.ejs`);
+    // res.render(`chatWidget.ejs`);
+    res.send(`WIP`)
 });
 
 module.exports = router;
