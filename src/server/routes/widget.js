3950c9be5df48dc6fc53f6e1030293de3820c481
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-02-06 13:16:49 +0530

    Chat widget works lol

--- src/server/routes/widget.js
+++ src/server/routes/widget.js
@@ -7,8 +7,8 @@ router.get(`/chat/:streamer`, async (req, res) => {
     const streamerData = await User.findOne({ username: req.params.streamer.toLowerCase() });
 
     if (!streamerData) return res.render(`404.ejs`);
-    // res.render(`chatWidget.ejs`);
-    res.send(`WIP`);
+    res.render(`widgets/chat.ejs`);
+    // res.send(`WIP`);
 });
 
 module.exports = router;
