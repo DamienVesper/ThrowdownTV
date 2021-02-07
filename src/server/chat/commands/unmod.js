338a8b2587a9e5ac3b8c2cbb86389cef91abacf2
Damien Vesper <ldamienvesper@gmail.com>
2021-02-07 16:50:54 -0600

    add demote command and fix badges

---
+++ src/server/chat/commands/unmod.js
@@ -0,0 +1,20 @@
+const User = require(`../../models/user.model.js`);
+
+module.exports = {
+    description: `Demote a moderator.`,
+    aliases: [`demod`]
+};
+
+module.exports.run = async (message, args, chatter, chatUsers) => {
+    const userToUnmod = args.shift().toLowerCase();
+
+    const chatUser = await User.findOne({ username: chatter.username });
+    const userToUnmodExists = await User.findOne({ username: userToUnmod });
+
+    if (!userToUnmod || !userToUnmodExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
+    else if (!chatter.perms.streamer) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
+    else if (!chatUser.channel.moderators.includes(userToUnmod)) return chatter.emit(`commandMessage`, `That user is not a moderator of your channel!`);
+
+    chatUser.channel.moderators.splice(chatUser.channel.moderators.indexOf(userToUnmod), 1);
+    chatUser.save(() => chatter.emit(`commandMessage`, `Demoted ${userToUnmod} from a moderator of your channel.`));
+};
