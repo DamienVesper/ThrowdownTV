cbb7ccace3bb4dcd056573bf05ebc407641ee577
Damien Vesper <ldamienvesper@gmail.com>
2021-02-07 19:05:15 -0600

    verify arg is passed

--- src/server/chat/commands/mod.js
+++ src/server/chat/commands/mod.js
@@ -6,12 +6,13 @@ module.exports = {
 };
 
 module.exports.run = async (message, args, chatter, chatUsers) => {
+    if (!args[0]) return chatter.emit(`commandMessage`, `You must specify a user to promote!`);
     const userToMod = args.shift().toLowerCase();
 
     const chatUser = await User.findOne({ username: chatter.username });
     const userToModExists = await User.findOne({ username: userToMod });
 
-    if (!userToMod || !userToModExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
+    if (!userToModExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
     else if (!chatter.perms.streamer) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
     else if (chatter.username === userToMod) return chatter.emit(`commandMessage`, `You cannot promote yourself to a moderator!`);
     else if (chatUser.channel.moderators.includes(userToMod)) return chatter.emit(`commandMessage`, `That user is already a moderator of your channel!`);
--- src/server/chat/commands/unmod.js
+++ src/server/chat/commands/unmod.js
@@ -6,12 +6,13 @@ module.exports = {
 };
 
 module.exports.run = async (message, args, chatter, chatUsers) => {
+    if (!args[0]) return chatter.emit(`commandMessage`, `You must specify a user to demote!`);
     const userToUnmod = args.shift().toLowerCase();
 
     const chatUser = await User.findOne({ username: chatter.username });
     const userToUnmodExists = await User.findOne({ username: userToUnmod });
 
-    if (!userToUnmod || !userToUnmodExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
+    if (!userToUnmodExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
     else if (!chatter.perms.streamer) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
     else if (chatter.username === userToUnmod) return chatter.emit(`commandMessage`, `You cannot demote yourself from a moderator!`);
     else if (!chatUser.channel.moderators.includes(userToUnmod)) return chatter.emit(`commandMessage`, `That user is not a moderator of your channel!`);
