4120fa2bd20193c81d5b2121884debc8a8260511
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-02-08 11:33:24 +0530

    Finally...

--- src/server/chat/commands/ban.js
+++ src/server/chat/commands/ban.js
@@ -12,13 +12,13 @@ module.exports.run = async (message, args, chatter, chatUsers) => {
     const chatUser = await User.findOne({ username: chatter.username });
     const userToBanExists = await User.findOne({ username: userToBan });
 
-    if (!userToBanExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
-    else if (!chatter.perms.streamer || !chatter.perms.moderator) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
+    if (!(chatter.perms.moderator || chatter.perms.streamer)) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
+    else if (!userToBanExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
     else if (chatter.username === userToBan) return chatter.emit(`commandMessage`, `You cannot ban yourself!`);
     else if (chatUser.channel.bans.includes(userToBan)) return chatter.emit(`commandMessage`, `That user is already banned from your channel!`);
 
     chatUser.channel.bans.push(userToBan);
     chatUser.save(() => chatter.emit(`commandMessage`, `${userToBan} has been banned.`));
     const users = chatUsers.filter(user => user.channel === chatter.channel);
-    for (const user of users) user.emit(`broadcastMessage`, `${userToBan} was banned by a moderator.`);
+    for (const user of users) user.emit(`commandMessage`, `${userToBan} was banned by a moderator.`);
 };
