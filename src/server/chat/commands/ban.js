509de9000312b44325571a8eaa7aee2409e624f9
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-02-08 11:24:22 +0530

    brain fart

--- src/server/chat/commands/ban.js
+++ src/server/chat/commands/ban.js
@@ -13,7 +13,7 @@ module.exports.run = async (message, args, chatter, chatUsers) => {
     const userToBanExists = await User.findOne({ username: userToBan });
 
     if (!userToBanExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
-    else if (!chatter.perms.streamer) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
+    else if (!chatter.perms.streamer || !chatter.perms.moderator) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
     else if (chatter.username === userToBan) return chatter.emit(`commandMessage`, `You cannot ban yourself!`);
     else if (chatUser.channel.bans.includes(userToBan)) return chatter.emit(`commandMessage`, `That user is already banned from your channel!`);
 
--- src/server/chat/commands/unban.js
+++ src/server/chat/commands/unban.js
@@ -13,7 +13,7 @@ module.exports.run = async (message, args, chatter, chatUsers) => {
     const userToUnbanExists = await User.findOne({ username: userToUnban });
 
     if (!userToUnbanExists) return chatter.emit(`commandMessage`, `That user does not exist!`);
-    else if (!chatter.perms.streamer) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
+    else if (!chatter.perms.streamer || !chatter.perms.moderator) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
     else if (chatter.username === userToUnban) return chatter.emit(`commandMessage`, `You cannot unban yourself!`);
     else if (!chatUser.channel.bans.includes(userToUnban)) return chatter.emit(`commandMessage`, `That user is not banned from your channel!`);
 
