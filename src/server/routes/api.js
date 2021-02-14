674d8716ab01a59075b075eda43889adebfef4fd
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-14 18:31:31 +0530

    Update api.js

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -84,18 +84,14 @@ router.get(`/stream-key/:streamKey`, async (req, res) => {
 });
 
 router.get(`/streams`, async (req, res) => {
-    const streamerData = await User.find();
+    const streamerData = await User.collection.find().toArray();
     const streams = [];
 
     streamerData.forEach(async (streamer) => {
         const result = await axios.get(`https://us01.throwdown.tv/api/${streamer.username}`);
         console.log(`${streamer.username}-${result.data.isLive}`);
-        if (result.data.isLive === true) liveStreamer(streamer.username);
+        if (result.data.isLive === true) streams.push(streamer.username);
     });
-
-    function liveStreamer (streamer) {
-        streams.push(streamer);
-    }
 
     res.json(streams);
     console.log(streams);
