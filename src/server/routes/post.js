a01d001362b2bafd08a52247bfd31567e1d8eb2e
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-16 16:30:49 +0530

    Update post.js

--- src/server/routes/post.js
+++ src/server/routes/post.js
@@ -140,7 +140,8 @@ router.post(`/report/:streamer`, async (req, res) => {
         .setURL(`http://${config.domain}/${req.params.streamer.toLowerCase()}`)
         .setAuthor(`USER REPORT`, client.user.displayAvatarURL())
         .setDescription(`TOS Category: ${req.body[`tos-category`]}`)
-        .addField(`Description of Report`, req.body[`report-comments`]);
+        .addField(`Description of Report`, req.body[`report-comments`])
+        .setImage(`https://us01.throwdown.tv/thumbnail/${req.params.streamer.toLowerCase()}`);
     await channel.send(embed);
     res.json({ success: `Your Report was successfully sent, Redirecting you back...` });
 });
