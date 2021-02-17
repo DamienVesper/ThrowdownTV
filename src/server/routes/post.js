9cb079745e180d657854736a605fef21ca34751c
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-17 16:34:41 +0530

    Update post.js

--- src/server/routes/post.js
+++ src/server/routes/post.js
@@ -7,6 +7,7 @@ const { verify } = require(`hcaptcha`);
 
 // Discord
 const Discord = require(`discord.js`);
+const axios = require(`axios`);
 const client = new Discord.Client();
 client.login(process.env.DISCORD_BOT_TOKEN);
 
@@ -135,6 +136,7 @@ router.post(`/report/:streamer`, async (req, res) => {
     if (!streamer) return res.json({ errors: `Streamer does not exist.` });
     // if (!streamer.live) return res.json({ errors: `Streamer is not live, please contact us on Discord if it is serious.` });
     const channel = client.channels.cache.get(config.discordconfig.reportchannel);
+    await axios.get(`https://${streamer.settings.rtmpServer}.throwdown.tv/snapshot/${req.params.streamer.toLowerCase()}`);
     const embed = new Discord.MessageEmbed()
         .setTitle(`${req.params.streamer.toLowerCase()}`)
         .setColor(`#0099ff`)
@@ -149,7 +151,9 @@ router.post(`/report/:streamer`, async (req, res) => {
         )
         .setImage(`https://${streamer.settings.rtmpServer}.throwdown.tv/snapshot/${req.params.streamer.toLowerCase()}`)
         .setFooter(`Reported by: ${req.user.username}`);
-    await channel.send(embed);
+    setTimeout(() => {
+        channel.send(embed);
+    }, 1000);
     res.json({ success: `Your Report was successfully sent, redirecting...` });
 });
 
