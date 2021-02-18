f6e1654480f5399cb27c0647cb363de1f584008b
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-19 00:06:35 +0530

    fuck it

--- src/server/routes/post.js
+++ src/server/routes/post.js
@@ -4,26 +4,11 @@ const { randomString } = require(`../utils/random.js`);
 const User = require(`../models/user.model.js`);
 const config = require(`../../../config/config.js`);
 const { verify } = require(`hcaptcha`);
-const paypal = require(`paypal-rest-sdk`);
-const log = require(`../utils/log.js`);
-const Moment = require(`moment`);
 
 // Discord
 const Discord = require(`discord.js`);
 const client = new Discord.Client();
 client.login(process.env.DISCORD_BOT_TOKEN);
-
-const isoDate = new Date();
-isoDate.setSeconds(isoDate.getSeconds() + 4);
-// eslint-disable-next-line no-unused-expressions
-`${isoDate.toISOString().slice(0, 19)}Z`;
-
-// Paypal config
-paypal.configure({
-    mode: process.env.PAYPAL_ENV_SANDBOX,
-    client_id: process.env.PAYPAL_SANDBOX_CLIENT_ID,
-    client_secret: process.env.PAYPAL_SANDBOX_CLIENT_SECRET
-});
 
 // All POST requests are handled within this router (except authentication).
 router.post(`/dashboard`, async (req, res) => {
@@ -47,107 +32,6 @@ router.post(`/dashboard`, async (req, res) => {
     user.save(err => {
         if (err) return res.json({ errors: `Invalid user data` });
         return res.json({ success: `Succesfully updated stream data.` });
-    });
-});
-
-// Post for VIP Unsubscription
-router.post(`/vip/unsubscribe`, async (req, res) => {
-    if (!req.isAuthenticated()) return res.redirect(`/login`);
-
-    const user = await User.findOne({ username: req.user.username });
-
-    if (!user.perms.vip) return res.json({ errors: `You are not VIP` });
-});
-
-// Post for VIP Subscription
-router.post(`/vip/subscribe`, async (req, res) => {
-    if (!req.isAuthenticated()) return res.redirect(`/login`);
-
-    const user = await User.findOne({ username: req.user.username });
-    if (user.perms.vip) return res.json({ errors: `You are already VIP` });
-
-    const startDate = `${Moment(new Date()).add(10, `minute`).format(`gggg-MM-DDTHH:mm:ss`)}Z`;
-
-    const billingPlanAttributes = {
-        description: `Create Plan for Regular`,
-        merchant_preferences: {
-            auto_bill_amount: `yes`,
-            cancel_url: `http://localhost:8080/vip/cancel`,
-            initial_fail_amount_action: `continue`,
-            max_fail_attempts: `1`,
-            return_url: `http://localhost:8080/vip/success`
-        },
-        name: `ThrowdownTV VIP Membership`,
-        payment_definitions: [
-            {
-                amount: {
-                    currency: `USD`,
-                    value: `10`
-                },
-                cycles: `0`,
-                frequency: `MONTH`,
-                frequency_interval: `1`,
-                name: `Monthly Payment`,
-                type: `REGULAR`
-            }
-        ],
-        type: `INFINITE`
-    };
-
-    const billingPlanUpdateAttributes = [
-        {
-            op: `replace`,
-            path: `/`,
-            value: {
-                state: `ACTIVE`
-            }
-        }
-    ];
-
-    const billingAgreementAttributes = {
-        name: `Name of Payment Agreement`,
-        description: `Description of  your payment  agreement`,
-        start_date: startDate,
-        plan: {
-            id: ``
-        },
-        payer: {
-            payment_method: `paypal`
-        }
-    };
-
-    paypal.billingPlan.create(billingPlanAttributes, (error, billingPlan) => {
-        log(`cyan`, `Creating Billing Plan...`);
-        if (error) {
-            log(`red`, error);
-            res.json({ errors: error });
-        } else {
-            log(`green`, `Create Billing Plan Response`);
-            paypal.billingPlan.update(billingPlan.id, billingPlanUpdateAttributes, (error, response) => {
-                if (error) {
-                    log(`red`, error);
-                    res.json({ errors: error });
-                } else {
-                    billingAgreementAttributes.plan.id = billingPlan.id;
-                    paypal.billingAgreement.create(billingAgreementAttributes, (error, billingAgreement) => {
-                        if (error) {
-                            log(`red`, error);
-                            res.json({ errors: error });
-                        } else {
-                            billingAgreement.links.forEach(agreement => {
-                                if (agreement.rel === `approval_url`) {
-                                    // Redirecting to paypal portal with approvalUrl.
-                                    const approvalUrl = agreement.href;
-                                    const token = approvalUrl.split(`token=`)[1];
-                                    console.log(approvalUrl, token);
-                                    res.redirect(approvalUrl);
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }
     });
 });
 
