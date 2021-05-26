933fbd77eafa9889c89f022d867033c98b2bd5ab
Damien Vesper <ldamienvesper@gmail.com>
2021-05-26 09:58:36 -0500

    banlist tsify

--- src/server/chat/commands/banlist.ts
+++ src/server/chat/commands/banlist.ts
@@ -1,15 +1,21 @@
 import User from '../../models/user.model';
+import Chatter from '../socket';
 
 const config = {
     description: `Ban a user from chatting!`,
     aliases: [`bans`]
 };
 
-module.exports.run = async (message, args, chatter, chatUsers) => {
+const run = async (message: string, args: string[], chatter: Chatter, chatUsers: Chatter[]) => {
     const streamer = await User.findOne({ username: chatter.channel });
 
     if (!(chatter.perms.moderator || chatter.perms.streamer)) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
 
     if (streamer.channel.bans.length === 0) return chatter.emit(`commandMessage`, `Your ban list is empty, congrats!`);
     chatter.emit(`commandMessage`, `Banned Users: ${streamer.channel.bans}`);
+};
+
+export {
+    config,
+    run
 };
