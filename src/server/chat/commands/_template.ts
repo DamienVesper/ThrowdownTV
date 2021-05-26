61d63fbb3a85518d9c4f7f52a9da7dbe4f1453f9
Damien Vesper <ldamienvesper@gmail.com>
2021-05-25 22:18:01 -0500

    do the template

---
+++ src/server/chat/commands/_template.ts
@@ -0,0 +1,14 @@
+const config = {
+    description: `An amazing command!`,
+    aliases: [],
+    usage: ``
+};
+
+const run = async (message: string, args: string[], chatter, chatUsers: any[]) => {
+
+};
+
+export {
+    config,
+    run
+};
