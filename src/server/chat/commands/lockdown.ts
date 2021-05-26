1c27b59b1ac55a209a1ea8d1a433ad335e68e947
Damien Vesper <ldamienvesper@gmail.com>
2021-05-26 10:04:50 -0500

    lockdown cmd tsify

--- src/server/chat/commands/lockdown.ts
+++ src/server/chat/commands/lockdown.ts
@@ -1,18 +1,19 @@
-const User = require(`../../models/user.model.js`);
+import User from '../../models/user.model';
+import Chatter from '../socket';
 
-module.exports = {
-    description: `Ban a user from chatting!`,
-    aliases: [],
+const config = {
+    description: `Disable chatting on your channel!`,
     usage: `<on/off>`
 };
 
-module.exports.run = async (message, args, chatter, chatUsers) => {
+const run = async (message: string, args: string[], chatter: Chatter, chatUsers: Chatter[]) => {
     const value = args.shift().toLowerCase();
 
     const streamer = await User.findOne({ username: chatter.channel });
 
     if (!(chatter.perms.moderator || chatter.perms.streamer)) return chatter.emit(`commandMessage`, `You do not have permission to do that!`);
     if (!(value === `on` || value === `off`)) return chatter.emit(`commandMessage`, `Invalid Arguments! Usage: /lockdown <on/off>`);
+
     if (streamer.settings.lockdown === true && value === `on`) return chatter.emit(`commandMessage`, `Lockdown is already enabled!`);
     if (streamer.settings.lockdown === false && value === `off`) return chatter.emit(`commandMessage`, `Lockdown is already disabled!`);
 
@@ -23,4 +24,9 @@ module.exports.run = async (message, args, chatter, chatUsers) => {
 
     const users = chatUsers.filter(user => user.channel === chatter.channel && user.username !== chatter.username);
     for (const user of users) user.emit(`commandMessage`, `Chat Lockdown was turned ${value} by a moderator.`);
+};
+
+export {
+    config,
+    run
 };
