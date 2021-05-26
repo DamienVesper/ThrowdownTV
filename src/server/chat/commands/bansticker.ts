62ea3a67248761e6eb5526da109a6e39726f68e2
Damien Vesper <ldamienvesper@gmail.com>
2021-05-26 09:59:40 -0500

    bansticker tsify

--- src/server/chat/commands/bansticker.ts
+++ src/server/chat/commands/bansticker.ts
@@ -1,12 +1,13 @@
-const Sticker = require(`../../models/sticker.model.js`);
+import Sticker from '../../models/sticker.model';
+import Chatter from '../socket';
 
-module.exports = {
+const config = {
     description: `Ban a sticker from the channel!`,
     aliases: [`bs`],
-    usage: `<stickername>`
+    usage: `<stickerName>`
 };
 
-module.exports.run = async (message, args, chatter, chatUsers) => {
+const run = async (message: string, args: string[], chatter: Chatter, chatUsers: Chatter[]) => {
     const sticker = args.shift().toLowerCase();
 
     const stickerToBan = await Sticker.findOne({ stickerName: sticker });
@@ -19,4 +20,9 @@ module.exports.run = async (message, args, chatter, chatUsers) => {
 
     const users = chatUsers.filter(user => user.channel === chatter.channel && user.username !== chatter.username);
     for (const user of users) user.emit(`commandMessage`, `Sticker "${sticker}" was banned by a moderator.`);
+};
+
+export {
+    config,
+    run
 };
