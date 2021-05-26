13b2171edc27c52f08518a7b864ebe683e0ab7d7
Damien Vesper <ldamienvesper@gmail.com>
2021-05-25 22:07:26 -0500

    RAJEEV WTF; tsify page router

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,148 +1,166 @@
-const express = require(`express`);
-const router = express.Router();
-const User = require(`../models/user.model.js`);
-const Ban = require(`../models/ban.model.js`);
+import * as Express from 'express';
+
+import User from '../models/user.model';
+import Ban from '../models/ban.model';
+
+const pageRouter: Express.Router = Express.Router();
 
 // Landing page.
-router.get(`/`, async (req, res) => {
+pageRouter.get(`/`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     req.isAuthenticated()
         ? res.redirect(`/browse`)
         : res.render(`welcome.ejs`);
 });
 
 // Sign Up / login pages.
-router.get(`/signup`, async (req, res) => {
+pageRouter.get(`/signup`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     req.isAuthenticated()
         ? res.redirect(`/`)
         : res.render(`signup.ejs`);
 });
-router.get(`/login`, async (req, res) => {
+
+pageRouter.get(`/login`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     req.isAuthenticated()
         ? res.redirect(`/`)
         : res.render(`login.ejs`);
 });
 
 // Terms of Service.
-router.get(`/tos`, async (req, res) => {
+pageRouter.get(`/tos`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     res.render(`tos.ejs`);
 });
 
 // Browsing.
-router.get(`/browse`, async (req, res) => {
+pageRouter.get(`/browse`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     res.render(`browse.ejs`);
 });
 
 // Staff
-router.get(`/staff`, async (req, res) => {
+pageRouter.get(`/staff`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     res.render(`staff.ejs`);
 });
 
 // Admin Panel
-router.get(`/admin`, async (req, res) => {
+pageRouter.get(`/admin`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     if (!req.isAuthenticated()) return res.redirect(`/login`);
-    const accessingUser = await User.findOne({ username: req.user.username });
+
+    const accessingUser = await User.findOne({ username: (<any>req).user.username });
+
     if (!accessingUser.perms.staff) res.send(`You must be an administrator to access this page!`);
     else res.render(`admin.ejs`);
 });
 
 // Following.
-router.get(`/following`, async (req, res) => {
+pageRouter.get(`/following`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     if (!req.isAuthenticated()) return res.redirect(`/login`);
 
     res.render(`following.ejs`);
 });
 
 // Dashboard.
-router.get(`/dashboard`, async (req, res) => {
+pageRouter.get(`/dashboard`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
     if (!req.isAuthenticated()) return res.redirect(`/login`);
 
-    const user = await User.findOne({ username: req.user.username });
+    const user = await User.findOne({ username: (<any>req).user.username });
     if (!user) return res.redirect(`/logout`);
 
     res.render(`dashboard.ejs`, {
         user: user.username,
-        streamTitle: user.streamTitle,
-        streamKey: user.canStream ? user.streamKey : `You do not have permission to stream.`,
-        streamDescription: user.streamDescription,
+        streamTitle: user.settings.title,
+        streamKey: user.settings.lockdown ? user.settings.streamKey : `You do not have permission to stream.`,
+        streamDescription: user.settings.description,
         streamAvatar: user.avatarURL,
-        donationLink: user.donationLink,
-        discordID: user.discordID
+        donationLink: user.settings.donationLink
     });
 });
 
 // Chat
-router.get(`/chat/*`, async (req, res) => {
+pageRouter.get(`/chat/*`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     const user = await User.findOne({ username: req.url.split(`/`)[2].toLowerCase() });
     if (!user) return res.redirect(`/`);
 
     res.render(`chat.ejs`);
 });
 
-router.get(`/vip`, async (req, res) => {
+pageRouter.get(`/vip`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     if (!req.isAuthenticated()) return res.redirect(`/login`);
-    const user = await User.findOne({ username: req.user.username });
+    const user = await User.findOne({ username: (<any>req).user.username });
 
     if (!user.perms.vip) return res.render(`subscribeVIP.ejs`);
     else return res.render(`alreadyVIP.ejs`);
 });
 
-router.get(`/vip/success`, async (req, res) => {
+pageRouter.get(`/vip/success`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     if (!req.isAuthenticated()) return res.redirect(`/login`);
-    console.log(req.query);
     res.send(`Success`);
 });
 
-router.get(`/vip/cancel`, async (req, res) => {
+pageRouter.get(`/vip/cancel`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     if (!req.isAuthenticated()) return res.redirect(`/login`);
-    console.log(req.query);
     res.send(`Error`);
 });
 
-router.get(`/report/:streamer`, async (req, res) => {
+pageRouter.get(`/report/:streamer`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
+
     if (!req.isAuthenticated()) return res.redirect(`/login`);
+
     const streamer = await User.findOne({ username: req.params.streamer.toLowerCase() });
     if (!streamer) return res.json({ errors: `Streamer does not exist.` });
+
     res.render(`report.ejs`);
 });
 
-router.get(`/:streamer`, async (req, res) => {
+pageRouter.get(`/:streamer`, async (req: Express.Request, res: Express.Response) => {
     const ip = await Ban.findOne({ IP: req.ip });
     if (ip) return res.send(`IP: ${req.ip} is blocked from accessing this page.`);
-    const streamer = req.params.streamer;
 
+    const streamer = req.params.streamer;
     const user = await User.findOne({ username: streamer.toLowerCase() });
+
     if (!user) return res.render(`404.ejs`);
     if (user.isSuspended) return res.render(`banned.ejs`);
 
     res.render(`streamer.ejs`);
 });
 
-module.exports = router;
+export default pageRouter;
