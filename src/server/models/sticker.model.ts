ab08563441450b29145f98c243dffdc1b53bf9f7
Damien Vesper <ldamienvesper@gmail.com>
2021-06-02 11:13:19 -0500

    condense sticker model

--- src/server/models/sticker.model.ts
+++ src/server/models/sticker.model.ts
@@ -2,23 +2,10 @@ import * as Mongoose from 'mongoose';
 import { StickerDoc } from '../types/models';
 
 const stickerSchema = new Mongoose.Schema({
-    stickerName: {
-        type: String,
-        required: true
-    },
-    ownerUsername: {
-        type: String,
-        required: true
-    },
-    path: {
-        type: String,
-        required: true
-    },
-    channelsBannedOn: {
-        type: Array,
-        default: [],
-        required: false
-    }
+    stickerName: { type: String, required: true },
+    ownerUsername: { type: String, required: true },
+    path: { type: String, required: true },
+    channelsBannedOn: { type: Array, required: false, default: [] }
 });
 
 const Sticker = Mongoose.model<StickerDoc>(`Sticker`, stickerSchema);
