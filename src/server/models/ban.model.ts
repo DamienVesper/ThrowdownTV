2aee9a2103844d3a79ae5bdc131a718ccce98256
Damien Vesper <ldamienvesper@gmail.com>
2021-06-02 11:13:38 -0500

    condense ban model

--- src/server/models/ban.model.ts
+++ src/server/models/ban.model.ts
@@ -2,14 +2,8 @@ import * as Mongoose from 'mongoose';
 import { BanDoc } from '../types/models';
 
 const banSchema = new Mongoose.Schema({
-    IP: {
-        type: String,
-        required: true
-    },
-    comment: {
-        type: String,
-        required: false
-    }
+    IP: { type: String, required: true },
+    comment: { type: String, required: false }
 });
 
 const Ban = Mongoose.model<BanDoc>(`Ban`, banSchema);
